d93327e60aea0ed73ae513c1c44ed6ad2e1c6bbc
Bibekl-lamichhane <bibek1999lamichhane@gmail.com>
2024-06-06 01:12:14 +0545

    inital commit on frontend

---
+++ FrontEnd/vite-project/src/components/Login.jsx
@@ -0,0 +1,113 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import axios from "axios";
+import toast from "react-hot-toast";
+function Login() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    const userInfo = {
+      email: data.email,
+      password: data.password,
+    };
+    await axios
+      .post("http://localhost:4001/user/login", userInfo)
+      .then((res) => {
+        console.log(res.data);
+        if (res.data) {
+          toast.success("Loggedin Successfully");
+          document.getElementById("my_modal_3").close();
+          setTimeout(() => {
+            window.location.reload();
+            localStorage.setItem("Users", JSON.stringify(res.data.user));
+          }, 1000);
+        }
+      })
+      .catch((err) => {
+        if (err.response) {
+          console.log(err);
+          toast.error("Error: " + err.response.data.message);
+          setTimeout(() => {}, 2000);
+        }
+      });
+  };
+  return (
+    <div>
+      <dialog id="my_modal_3" className="modal backdrop-blur-xl">
+        <div className="modal-box">
+          <form onSubmit={handleSubmit(onSubmit)} method="dialog">
+            {/* Button to  close the modal */}
+            <Link
+              to="/"
+              className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2  text-gray-600"
+              onClick={() => document.getElementById("my_modal_3").close()}
+            >
+              ✕
+            </Link>
+
+            <h3 className="text-center font-bold text-xl text-gray-950">
+              Login
+            </h3>
+            {/* email */}
+            <div className="mt-4 space-y-2">
+              <span className="ml-2  text-gray-950">Email</span>
+              <br />
+              <input
+                type="email"
+                placeholder="Your Email"
+                className="w-80 px-3 py-2 border rounded-3xl outline-none"
+                {...register("email", { required: true })}
+              />
+              <br />
+              {errors.email && (
+                <span className="text-sm text-red-500 m-2">
+                  Email is required
+                </span>
+              )}
+            </div>
+            {/* password */}
+            <div className="mt-4 space-y-2">
+              <span className="ml-2  text-gray-950">Password</span>
+              <br />
+              <input
+                type="password"
+                placeholder="Password"
+                className="w-80 px-3 py-2 border rounded-3xl outline-none"
+                {...register("password", { required: true })}
+              />
+              <br />
+              {errors.password && (
+                <span className="text-sm text-red-500 m-2">
+                  Password Cannot be Empty
+                </span>
+              )}
+            </div>
+
+            {/* Button */}
+            <div className="flex justify-around mt-6">
+              <button className="bg-blue-500 text-white  rounded-2xl px-4 py-1 hover:bg-blue-600 duration-200">
+                Login
+              </button>
+              <p className=" text-gray-950">
+                Dont have account?{" "}
+                <Link
+                  to="/signup"
+                  className="underline text-blue-500 cursor-pointer"
+                >
+                  Signup
+                </Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </dialog>
+    </div>
+  );
+}
+
+export default Login;
